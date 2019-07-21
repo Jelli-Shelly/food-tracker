cc4bf2e23de95345c9bf2e70edc89aee1c0484b8
Shellina Magarzo <shellina_magarzo@yahoo.com>
2019-07-21 04:17:53 +0000

    edited list buttons to outline

--- food_tracker/food_tracker/settings.py
+++ food_tracker/food_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+^pkrorfc!e+py*7we^^13jio5c7manjvw1ivwi(iy$*&#-^sv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jellishelly.pythonanywhere.com']
 
 
 # Application definition
